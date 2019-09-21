09841b708f21e9b54f615553b40707aab9637c5a
Jørn Ove Stokka <jorn.ove.stokka@egmont.com>
2019-09-21 18:58:10 +0200

    Testing 2

--- foyer.php
+++ foyer.php
@@ -10,7 +10,7 @@
  *
  * @link              https://mennoluitjes.nl
  * @since             1.0.0
- * @package           FoyerX
+ * @package           FoyerXX
  *
  * @wordpress-plugin
  * Plugin Name:       Foyer - Digital Signage for WordPress
